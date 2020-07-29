6036d2fe8a6ba2a4ea729496adf4019d5fe724a9
chanders1078 <shekharsaa@gmail.com>
2020-07-29 22:27:31 +0530

    Covid Alert Box

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var btn = $(".close-btn");
+    $(btn).on("click", function() {
+        $(btn).parent().hide();
+    });
+    
+    var stickyNavTop = $('.container-fluid').offset().top;
+    var stickyNav = function(){
+    var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > stickyNavTop) { 
+          $('.container-fluid').addClass('sticky');
+        } else {
+          $('.container-fluid').removeClass('sticky'); 
+        }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function() {
+        stickyNav();
+     });
+});
