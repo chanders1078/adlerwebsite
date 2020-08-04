0e98c9d7ebc5406d650379b77c04fb8ff41137ef
chanders1078 <shekharsaa@gmail.com>
2020-08-04 22:23:07 +0530

    final-layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     $(btn).on("click", function() {
         $(btn).parent().hide();
     });
-    
+    /* Sticky Nvabar 
+    */
     var stickyNavTop = $('.container-fluid').offset().top;
     var stickyNav = function(){
     var scrollTop = $(window).scrollTop();
@@ -19,4 +20,30 @@ $(document).ready(function() {
     $(window).scroll(function() {
         stickyNav();
      });
+     
+     $("#sbtn1").on("click", function() {
+        $(".solution-icon-box span").css({ "backgroundColor" : "#fdd600" , "border" : "0"});
+        $(".solution-content-col1 img").attr('src','../assets/images/25-yd-dewatering-canvas-tarp.jpg');
+     });
+     $("#sbtn2").on("click", function() {
+        $(".solution-icon-box span").css({ "backgroundColor" : "#fdd600" , "border" : "0"});
+        $(".solution-content-col1 img").attr('src','../assets/images/21000-round-bottom-closed-top-tank.png');
+      });
+      $("#sbtn3").on("click", function() {
+        $(".solution-icon-box span").css({ "backgroundColor" : "#fdd600" , "border" : "0"});
+        $(".solution-content-col1 img").attr('src','../assets/images/onroad-tank.jpg');
+     });
+     $("#sbtn4").on("click", function() {
+        $(".solution-icon-box span").css({ "backgroundColor" : "#fdd600" , "border" : "0"});
+        $(".solution-content-col1 img").attr('src','../assets/images/21000-round-bottom-closed-top-tank.png');
+       });
+    
+    $(".collapse-btn").on("click" , function() {
+        $(".mobile-nav").show();
+    });   
+
+    $(".closebtn").on("click", function() {
+        $(".mobile-nav").hide();
+    });
+    
 });
